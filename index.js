765e328c96ce3723130797ad6cce7ce32b19439a
cynthianyeint <nyeintnyeintkhin.92@gmail.com>
2018-07-06 17:49:33 +0800

    add npm

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
+    res.end('Hello World! - index.js');
     res.write(req.url);
-}).listen(8080);
+}).listen(8000);
